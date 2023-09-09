d88c0b1dd41ed00880a9f4e5e9ec6117df7a5541
bug_patch152 <81291106+Gaming1010@users.noreply.github.com>
2023-09-09 16:14:48 +0400

    Add files via upload

---
+++ public/chat/js/main.js
@@ -0,0 +1,100 @@
+const chatForm = document.getElementById('chat-form');
+const socket = io();
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+const commands = ['!req']
+const chatMessages = document.querySelector('.chat-messages');
+
+
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+})
+
+//returns to /chat/ if the username OR room name is undefined
+if (username == undefined || room == undefined) {
+  location.href = "/chat/";
+}
+
+//function to Join room 
+function joinroom() {
+  socket.emit('joinRoom', { username, room })
+}
+socket.emit('request_who_online')
+
+// Get room and users
+socket.on('roomUsers', ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+})
+
+socket.on('message', message => {
+  outputMessage(message);
+
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+chatForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const msg = event.target.elements.msg.value;
+  if (!commands.includes(msg)) {
+    socket.emit('chatMessage', msg);
+  } else if (msg == commands[0]) {
+    socket.emit('request_who_online');
+  }
+
+  event.target.elements.msg.value = '';
+  event.target.elements.msg.focus();
+})
+
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = `
+    <p class="meta">${message.username} |
+    <span></span>${message.time}</p>
+    <p class="text">${message.text}</p>`;
+  document.querySelector('.chat-messages').appendChild(div)
+}
+
+// add room to DOM
+function outputRoomName(room) {
+  roomName.innerText = room + " Chat-room";
+}
+
+//add users to DOM
+function outputUsers(users) {
+  console.log(users)
+  userList.innerHTML = `
+    ${users.map(user => `<li>${user.username}</li>`).join('')}`;
+}
+
+
+//on error, do something
+socket.on('error', error => {
+  if (error == "rejoin") {
+    joinroom()
+  }
+});
+
+socket.on('who_all_online', () => {
+  joinroom()
+})
+
+socket.on('disconnect', () => {
+  try {
+    socket.emit('request_who_online');
+  } catch {
+    reload();
+  }
+})
+socket.on('clear_old_msgs', () => {
+  try {
+    const boxes = Array.from(document.getElementsByClassName('message'));
+    boxes.forEach(box => {
+      box.remove();
+    });
+  } catch (error) {
+    console.log(error)
+  }
+})
