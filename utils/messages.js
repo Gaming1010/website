b7496e36b96469eebc469117443c2e83dacb6b06
Gaming1010 <81291106+Gaming1010@users.noreply.github.com>
2023-09-10 07:18:40 +0000

    Updated

--- utils/messages.js
+++ utils/messages.js
@@ -1,12 +1,12 @@
 const moment = require('moment');
 
 
-function formatMessage(username,text) {
-    return {
-        username,
-        text,
-        time : moment().format('MMMM DD dddd HH:mm a')
-    }
+function formatMessage(username, text) {
+  return {
+    username,
+    text,
+    time: moment().format('dddd DD MMM Y HH:mm a')
+  }
 }
 
 module.exports = formatMessage;
