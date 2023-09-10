d6f028ef38c25f33f42328c6fb72d2b5b6c2edbb
unknown <aslamhkaq9@gmail.com>
2023-09-10 19:33:00 +0400

    W-I-P

--- public/projects/game_files/game.js
+++ public/projects/game_files/game.js
@@ -1,3 +1,15 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = c.getContext("2d");
-const image = "";
+let canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext('2d');
+let img = new Image();
+img.src = "/projects/game_files/sky-bg.png";
+canvas.height = window.innerHeight
+canvas.width = window.innerWidth
+
+
+window.onload = function() {
+    function loop() {
+        ctx.drawImage(img,0,0,can,canvas.height);
+    }
+    loop();
+ 
+}
