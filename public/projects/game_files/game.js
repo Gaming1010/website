15eae988e0d820083040370e28d725d0326d44f8
unknown <aslamhkaq9@gmail.com>
2023-09-10 20:29:30 +0400

    small changes

--- public/projects/game_files/game.js
+++ public/projects/game_files/game.js
@@ -2,14 +2,62 @@ let canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext('2d');
 let img = new Image();
 img.src = "/projects/game_files/sky-bg.png";
-canvas.height = window.innerHeight
-canvas.width = window.innerWidth
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+function drawImageProp(ctx, img, x, y, w, h, offsetX, offsetY) {
+
+    if (arguments.length === 2) {
+        x = y = 0;
+        w = ctx.canvas.width;
+        h = ctx.canvas.height;
+    }
+
+    // default offset is center
+    offsetX = typeof offsetX === "number" ? offsetX : 0.5;
+    offsetY = typeof offsetY === "number" ? offsetY : 0.5;
+
+    // keep bounds [0.0, 1.0]
+    if (offsetX < 0) offsetX = 0;
+    if (offsetY < 0) offsetY = 0;
+    if (offsetX > 1) offsetX = 1;
+    if (offsetY > 1) offsetY = 1;
+
+    var iw = img.width,
+        ih = img.height,
+        r = Math.min(w / iw, h / ih),
+        nw = iw * r,   // new prop. width
+        nh = ih * r,   // new prop. height
+        cx, cy, cw, ch, ar = 1;
+
+    // decide which gap to fill    
+    if (nw < w) ar = w / nw;                             
+    if (Math.abs(ar - 1) < 1e-14 && nh < h) ar = h / nh;  // updated
+    nw *= ar;
+    nh *= ar;
+
+    // calc source rectangle
+    cw = iw / (nw / w);
+    ch = ih / (nh / h);
+
+    cx = (iw - cw) * offsetX;
+    cy = (ih - ch) * offsetY;
+
+    // make sure source rectangle is valid
+    if (cx < 0) cx = 0;
+    if (cy < 0) cy = 0;
+    if (cw > iw) cw = iw;
+    if (ch > ih) ch = ih;
+
+    // fill image in dest. rectangle
+    ctx.drawImage(img, cx, cy, cw, ch,  x, y, w, h);
+}
 
 
 window.onload = function() {
     function loop() {
-        ctx.drawImage(img,0,0,can,canvas.height);
+        /*ctx.drawImage(img,0,0,canvas.wid,canvas.height); */
+        drawImageProp(ctx, img, 0, 0, canvas.width, canvas.height,offsetX);
     }
     loop();
- 
 }
